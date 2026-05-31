c9b7202e2c477784550e1e8ed5c9a9e7b121babe
SUGAVANESWARA <Sugan18112002@gmail.com>
2026-05-31 22:29:00 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,150 @@
+''' ASSIGNMENT OPERATORS '''
+
+
+#1 Wallet ( -= )
+
+wallet = 500
+wallet -= 120
+print(wallet)
+
+
+#2 Salary ( += )
+
+salary = 0
+salary += 3000
+print(salary)
+
+
+#3 Water Tank ( += )
+
+capacity = 150
+capacity += 50
+print(capacity)
+
+
+#4 Battery ( -= )
+
+battery = 90
+battery -= 25
+print(battery)
+
+
+#5 Savings ( *= )
+
+savings = 2000
+savings *= 2
+print(savings)
+
+
+#6 Price of bag ( += )
+
+price = 850
+price += 150
+print(price)
+
+
+#7 Bike Travel ( += )
+
+distance = 120
+distance += 30
+print(distance)
+
+
+#8 Lose of points ( -= )
+
+points = 95
+lost = 15
+points -= lost
+print(points)
+
+
+#9 Number gets multiplied ( *= )
+
+number = 12
+number *= 5
+print(number)
+
+
+#10 Chocolate box ( -= )
+
+chocolates = 40
+sold = 10
+chocolates -= sold
+print(chocolates)
+
+
+#11 Account balance ( += )
+
+balance = 15000
+deposit = 5000
+balance += deposit
+print(balance)
+
+
+#12 Classroom ( -= )
+
+total_students = 60
+absent = 5
+total_students -= absent
+print(total_students)
+
+
+#13 Salary tripled ( *= )
+
+salary = 8000
+salary *= 3
+print(salary)
+
+
+#14 Train Ticket Price ( += )
+
+price = 950
+gst = 50
+price += gst
+print(price)
+
+
+#15 Cricket Score ( += )
+
+score = 120
+score += 35
+print(score)
+
+
+#16 Laptop Battery ( -= )
+
+battery = 100
+consumed = 40
+battery -= consumed
+print(battery)
+
+
+#17 Cows Doubled ( *= )
+
+cows = 25
+cows *= 2
+print(cows)
+
+
+#18 Pen shop ( -= )
+
+pens = 75
+sold = 20
+pens -= sold
+print(pens)
+
+
+#19 Mark increased ( += )
+
+marks = 72
+inc_marks  = 8
+marks += inc_marks
+print(marks)
+
+
+#20 Number multiplied ( *= )
+
+number = 7
+number *= 10
+print(number)
+
